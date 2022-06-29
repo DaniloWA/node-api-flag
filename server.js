c8c090d64aff0e54e1942513e287a053ef722383
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-06-29 21:33:33 +0100

    feat: create endpoint with params

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.listen(8080, () => {
+  console.log("Server running!");
+});
 
 app.get("/", (req, res) => {
   res.json("Server get");
+});
+
+//GET retorna produtos por ID
+app.get("/api/produtos/:id", (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const filteredID = data.filter((data) => {
+      return id === data.id;
+    });
+
+    if (filteredID.length === 0) {
+      return res.status(404).json("Id not found");
+    }
+
+    return res.status(200).json(filteredID);
+  } catch (error) {
+    return res.status(404).json(error);
+  }
 });
